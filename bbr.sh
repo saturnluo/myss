7455d563c1bdab45079ede66a8c3464876564726
luomingjun <luomingjun@chinatsp.com>
2019-09-26 13:53:45 +0800

    update

--- bbr.sh
+++ bbr.sh
@@ -1,6 +1,6 @@
-wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
-chmod +x shadowsocks-all.sh
-./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+#wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+#chmod +x shadowsocks-all.sh
+#./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
 # OR
 #yum -y install wget&&wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh&&chmod +x shadowsocksR.sh&&./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
 # ubuntu 使用shadowsocks
@@ -9,7 +9,7 @@ chmod +x shadowsocks-all.sh
 #||engadget.com^
 
 # TCP BBR 拥塞控制算法
-#wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh&&chmod +x bbr.sh&&./bbr.sh 2>$1 | tee bbr.log
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh&&chmod +x bbr.sh&&./bbr.sh 2>$1 | tee bbr.log
 
 # Kcptun 配置 udp加速
 #wget --no-check-certificate https://github.com/kuoruan/shell-scripts/raw/master/kcptun/kcptun.sh
--- kcptun.sh
+++ kcptun.sh
@@ -1,6 +1,6 @@
-wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
-chmod +x shadowsocks-all.sh
-./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+#wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+#chmod +x shadowsocks-all.sh
+#./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
 # OR
 #yum -y install wget&&wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh&&chmod +x shadowsocksR.sh&&./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
 # ubuntu 使用shadowsocks
@@ -12,6 +12,6 @@ chmod +x shadowsocks-all.sh
 #wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh&&chmod +x bbr.sh&&./bbr.sh 2>$1 | tee bbr.log
 
 # Kcptun 配置 udp加速
-#wget --no-check-certificate https://github.com/kuoruan/shell-scripts/raw/master/kcptun/kcptun.sh
-#chmod +x ./kcptun.sh
-#./kcptun.sh 2>$1 | tee kcptun.txt
+wget --no-check-certificate https://github.com/kuoruan/shell-scripts/raw/master/kcptun/kcptun.sh
+chmod +x ./kcptun.sh
+./kcptun.sh 2>$1 | tee kcptun.txt
